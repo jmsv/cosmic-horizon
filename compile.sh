69a3e8640421aeca48c7e1945341556d430b17ad
James Vickery <jamesevickery.dev@gmail.com>
2017-01-31 16:42:58 +0000

    Database test

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ src/game.cpp -o bin/game.o
+g++ src/game.cpp -l sqlite3 -o bin/game.o
--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <stdio.h>
+#include <sqlite3.h>
+
+int database_test()
+{
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  int rc;
+
+  rc = sqlite3_open("test.db", &db);
+
+  if( rc ){
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    return(0);
+  }else{
+    fprintf(stderr, "Opened database successfully\n");
+  }
+  sqlite3_close(db);
+  
+  return 0;
+}
 
 int main()
 {
+  database_test();
   std::cout << "Hello World!\n";
 }
