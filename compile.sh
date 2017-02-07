79e6a4169812fe8a0c23c331a1d6fe223213401b
James Vickery <thejamesvickery@gmail.com>
2017-02-07 13:28:14 +0000

    Improved compile script with linting

--- compile.sh
+++ compile.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
 # Running this (./compile.sh) uses the below command to compile to bin/game.o
 
+python cpplint.py src/*
+rc=$?; if [[ $rc != 0 ]];
+then
+    echo -e
+    read -e -p "Linting warnings. Enter 'y' to abort : " choice
+    if [ "$choice" == "y" ]; then
+        echo "Aborting"
+        exit $rc;
+    else
+        echo "Compiling..."
+    fi
+
+fi
+
 g++ src/userin.cpp src/dbstore.cpp src/print.cpp src/game.cpp -l sqlite3 -o bin/game.o
+rc=$?; if ! [[ $rc != 0 ]];
+then
+    echo "Compiled successfully"
+fi
