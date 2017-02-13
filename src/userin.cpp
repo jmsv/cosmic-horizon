08909961aed1e39fc5bae7df3a8718d21f144ce5
Deyan Bozhilov <bozhilo2@uni.coventry.ac.uk>
2017-02-13 11:03:45 +0000

    Yes or no function

--- src/userin.cpp
+++ src/userin.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <string>
+#include "print.h"
 using namespace std;
 
 
 bool valid_name(string test_name)
 {
   return true;
+}
+
+
+bool yes_no()
+{
+    cout << "Yes or no?";
+    string answer = "";
+    bool valid_in = false;
+    while(!valid_in)
+    {
+        cin >> answer;
+        if (answer == "y" || answer == "yes" || answer == "Y" || answer == "Yes")
+        {
+            valid_in = true;
+            return true;
+        }
+        else if(answer == "n" || answer == "no" || answer == "N" || answer == "No")
+        {
+            valid_in = true;
+            return false;
+        }
+        else
+        {
+            cout << "Invalid input! Try again";
+        }
+    }
 }
 
 
