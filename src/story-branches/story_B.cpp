4537aabace5e4d66d2eb9f4019dbd7341ab16dbc
bahar6 <baharx666@gmail.com>
2017-03-01 15:01:55 +0000

    Just a few stories created to the Story_B

--- src/story-branches/story_B.cpp
+++ src/story-branches/story_B.cpp
@@ -2,8 +2,63 @@
 #include <string>
 using namespace std;
 
-int start_B()
+void ship_gone()
+{}
+
+void mobile_phone_found()
+{}
+
+
+void found_help()
 {
-    // Put your story code here
-    return 0;
+    print("On the island, you have found a small abanded house.\n
+          "You start to explore the house to find a way to communicate\n
+          "to you leader back home");
+    print("Whilst exploring the house, you see a room filled with weapons");
+    print("What do you do?");
+    
+    print("1. Take some of the weapons and go back to your ship");
+    print("2. Leave the weapons and carry on searching for a communication method");
+    print("3. Use on the guns to shoot yourself")
+    
+    int choice = int_choice(3);
+    
+    if (choice== 1) {
+        ship_gone
+    } else if (choice==2) {
+        mobile_phone_found();
+    } else if (choice==3) {
+        print("\n You are now dead, Game over", "Red");
+    }
+    
+
+
+}
+
+
+
+
+void start_B()
+{
+    //Just starters 
+    print("You wake up to find that you are on a\n
+          "wrecked ship, on a deserted island");
+    print("You get up and see something in the distant");
+    print("Do you....?");
+    
+    print("1. Walk towards the object");
+    print("2. Run the opposite direction to find help");
+    print("3. Piss yourself and cry in your ship");
+    
+    int choice = int_choice(3);
+    
+    if (choice== 1) {
+        print("\nYou see that the object is moving towards you whilst holding a odd looking weapon in its hand. It starts shooting at you and you are now dead","Red");
+    } else if (choice==2) {
+        found_help();
+    } else if (choice==3) {
+        print("\n You have been crying for a week and you eventually die from the smell of your piss. Game over", "Red");
+    }
+    
+    
 }
