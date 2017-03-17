5ac0dc43c359a2804124a5c08235e31ea66c1ea4
JustLukeHere <ryanl9@uni.coventry.ac.uk>
2017-03-17 22:05:53 +0000

    Luke 2.0

--- src/story-branches/story_L.cpp
+++ src/story-branches/story_L.cpp
@@ -6,6 +6,8 @@
 #include <string>
 using namespace std;
 
+Database db_sE = Database();
+
 void discovering_base();
 void patrol_unit();
 void drone();
@@ -14,18 +16,19 @@ void kidnap();
 
 void start_L()
 {
+	StoryItem* start_LItem = new StoryItem();
     //Start 1.0
-    print("You see a distant gleam of metal not so far from where you are. a\n"
-          "As you make your way towards it you realise it is part of a massive group of structures",
-        "Magenta");
-    print("You wonder what plan you should go with now.", "Magenta");
-    print("Do you....?", "Magenta");
-
-    print("1. Sneak towards the buildings", "Blue");
-    print("2. Hide where you are and wait for movement from the buildings", "Blue");
-    print("3. Make your way back to the ship to gather supplies", "Blue");
-
-    int choice = int_option(3);
+    start_LItemItem->preText = "You see a distant gleam of metal not so far from where you are. a\n";
+    start_LItemItem->preText = "As you make your way towards it you realise it is part of a massive group of structures";
+    start_LItemItem->preTextColour = "magenta";
+    start_LItemItem->preText = "You wonder what plan you should go with now.", "Magenta";
+    start_LItemItem->prompt = "Do you:";
+    start_LItemItem->options = {
+		"1. Sneak towards the buildings",
+        "2. Hide where you are and wait for movement from the buildings",
+        "3. Make your way back to the ship to gather supplies"
+    };
+	int choice = start_LItem->run();
 
     if (choice == 1) {
         discovering_base();
@@ -38,32 +41,37 @@ void start_L()
 
 void discovering_base()
 {
-    //start
-    print("You make your way towards the base while keeping an eye out for any movement. a\n"
-          "While sneaking into the base you see figures swiftly walking nearby.",
-        "Magenta");
-    print("You are curious about where they are going.", "Magenta");
-    print("Do you....?", "Magenta");
-
+	StoryItem* discovering_baseItem = new StoryItem();
+    //start user exploring the base
+    start_discovering_baseItem->preText = "You make your way towards the base while keeping an eye out for any movement. a\n";
+    start_discovering_baseItem->preText = "While sneaking into the base you see figures swiftly walking nearby.";
+    start_discovering_baseItem->preText = "You are curious about where they are going.", "Magenta";
+    start_LItemItem->prompt = "Do you:";
+    start_LItemItem->options = {
+		"1. Attack the guards",
+        "2. Sneak in the other direction",
+        "3. Silently follow the guards"
+    };
     print("1. Attack the guards", "Blue");
     print("2. Sneak in the other direction", "Blue");
     print("3. Silently follow the guards", "Blue");
-
-    int choice = int_option(3);
+	int choice = discovering_baseItem->run();
 
     if (choice == 1) {
         print("The guards overpower you and wrestle you to the ground. After a short struggle you get knocked out");
         kidnap();
     } else if (choice == 2) {
-        discovering_ship();
+        print("The guards hear you and chase you down before knocking you out")
+        kidnap();
     } else if (choice == 3) {
+		print("You are not as silent as you think you are and the guards hear you, they catch you and knock you out")
         kidnap();
     }
 }
 
 void patrol_unit()
 {
-    //start
+    //start patrol unit finding the user
     print("Story. a\n"
           "Story",
         "Magenta");
@@ -87,7 +95,7 @@ void patrol_unit()
 
 void drone()
 {
-    //start
+    //start drone attacking ship
     print("You are nearing your ship when you see a patrol drone going directly towards your ship. a\n"
           "Story",
         "Magenta");
@@ -111,7 +119,7 @@ void drone()
 
 void kidnap()
 {
-    //start
+    //start user gets kidnapped
     print("You wake up with a sharp pain in your head. a\n"
           "Story",
         "Magenta");
@@ -135,7 +143,7 @@ void kidnap()
 
 void discovering_ship()
 {
-    //start
+    //start user discovers the giant ship
     print("Story. a\n"
           "Story",
         "Magenta");
@@ -158,6 +166,30 @@ void discovering_ship()
 }
 
 void newstory()
+{
+    //start
+    print("Story. a\n"
+          "Story",
+        "Magenta");
+    print("Story.", "Magenta");
+    print("Do you....?", "Magenta");
+
+    print("1. Option1", "Blue");
+    print("2. Option2", "Blue");
+    print("3. Option2", "Blue");
+
+    int choice = int_option(3);
+
+    if (choice == 1) {
+        discovering_base();
+    } else if (choice == 2) {
+        patrol_unit();
+    } else if (choice == 3) {
+        drone();
+    }
+}
+
+void newstory2()
 {
     //start
     print("Story. a\n"
